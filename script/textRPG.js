fd4946cace6825a50e3d46f5a7507500502438d0
Hiros-PC\Hiros <ja00800@nate.com>
2018-01-19 18:00:21 +0900

    textRPG 0.2.0

--- script/textRPG.js
+++ script/textRPG.js
@@ -1,112 +1,115 @@
-function logMessage(msg, color) {
-  if (!color) {
-    color = 'black';
-  }
-  var div = document.createElement('div');
-  div.innerHTML = msg;
-  div.style.color = color;
-  document.getElementById('log').appendChild(div);
-}
+var Game = (function() {
+  var instance;
+  var init = function(hero_name) {
+    var hero = {
+      name : hero_name,
+      lv : 1,
+      maxHp : 100,
+      hp : 100,
+      maxXp : 100,
+      xp : 0,
+      atk : 10
+    };
 
-logMessage('게임을 시작합니다!');
+    return {
+      showName : function() {
+        document.getElementById('hero_name').innerHTML = hero.name;
+        return this;
+      },
+      showLevel : function() {
+        document.getElementById('hero_level').innerHTML = 'lv.' + hero.lv;
+        return this;
+      },
+      showHp : function() {
+        if(hero.hp <= 0) {
+          return this.gameOver();
+        }
+        document.getElementById('hero_hp').innerHTML = 'HP : ' + hero.hp + '/' + hero.maxHp;
+        return this;
+      },
+      showXp : function() {
+        var self = this;
 
-var gameover = false;
-var battle = false;
+        if(hero.xp >= hero.maxXp) {
+          hero.xp -= hero.maxXp;
+          hero.maxHp += 10;
+          hero.hp = hero.maxHp;
+          hero.atk += hero.lv++;
+          window.setTimeout(function() {
+            self.setMessage('레벨업!');
+          }, 1000);
+        }
 
-function Character(name, hp, atk) {
-  this.name = name;
-  this.hp = hp;
-  this.atk = atk;
-}
+        document.getElementById('hero_xp').innerHTML = 'XP : ' + hero.xp + '/' + hero.maxXp;
+        return this;
+      },
+      showAtk : function() {
+        document.getElementById('hero_atk').innerHTML = 'ATK : ' + hero.atk;
+        return this;
+      },
+      showHeroStat : function() {
+        return this.showName().showLevel().showHp().showXp().showAtk();
+      },
+      setMessage : function(msg) {
+        document.getElementById('message').innerHTML = msg + '</br>';
+        return this;
+      },
+      showGameScreen : function() {
+        document.getElementById('menu_battle').style.display = 'none';
+        document.getElementById('menu_game').style.display = 'block';
+        document.getElementById('input_menu').focus();
+        return this;
+      },
+      showBattleScreen : function() {
+        document.getElementById('menu_game').style.display = 'none';
+        document.getElementById('menu_battle').style.display = 'block';
+        document.getElementById('input_battle').focus();
+        return this;
+      },
+      toggleMenu : function() {
+        //this.showHeroStat();
+        document.getElementById('screen_start').style.display = 'none';
 
-Character.prototype.attacked = function(damage) {
-  this.hp -= damage;
-  logMessage(this.name + '의 체력이 ' + this.hp + '가 되었습니다.');
-  if(this.hp <= 0) {
-    battle = false;
-  }
-};
-
-Character.prototype.attack = function(target) {
-  logMessage(this.name + '이(가) ' + target.name + '을(를) 공격합니다!');
-  target.attacked(this.atk);
-};
-
-//Character 상속
-function Hero(name, hp, atk, lv, xp) {
-  Character.apply(this, arguments);
-  this.lv = lv || 1;
-  this.xp = xp || 0;
-}
-Hero.prototype = Object.create(Character.prototype);
-Hero.prototype.constructor = Hero;
-
-Hero.prototype.attacked = function(damage) {
-  this.hp -= damage;
-  logMessage(this.name + '의 체력이 ' + this.hp + '가 되었습니다.');
-  if(this.hp <= 0) {
-    logMessage(this.name + '님이 사망하셨습니다. (Lv : ' + this.lv + ')', 'red');
-    battle = false;
-    gameover = true;
-  }
-};
-
-Hero.prototype.attack = function(target) {
-  logMessage(this.name + '님이 ' + target.name + '을(를) 공격합니다!');
-  target.attacked(this.atk);
-  if(target.hp <= 0) {
-    this.win(target);
-  }
-};
-
-Hero.prototype.win = function(target) {
-  logMessage('전투에서 승리하여 ' + target.xp + '의 경험치를 획득합니다(Lv : ' + this.lv + ' HP : ' + this.hp + ' ATK : ' + this.atk + ')', 'blue');
-  this.xp += target.xp;
-
-  if(this.xp >= 100 + 10 * this.lv) {
-    this.lv++;
-    logMessage(this.lv + '레벨이 되었습니다.', 'blue');
-    this.hp = 100 + 10 * this.lv;
-    this.xp -= 100 + 10 * this.lv;
-    this.atk++;
-  }
-};
-
-function Monster(name, hp, atk, lv, xp) {
-  Character.apply(this, arguments);
-  this.lv = lv || 1;
-  this.xp = xp || 10;
-}
-Monster.prototype = Object.create(Character.prototype);
-Monster.prototype.constructor = Monster;
-
-function encounter() {
-  var monsterArray = [
-    ['토끼', 25, 3, 1, 35],
-    ['늑대', 50, 6, 2, 50],
-    ['고블린', 80, 4, 3, 75],
-    ['해골', 120, 9, 4, 110],
-    ['디아블로', 500, 25, 6, 250]
-  ];
-
-  var monster = monsterArray[Math.floor(Math.random() * 5)];
-  return new Monster(monster[0], monster[1], monster[2], monster[3], monster[4]);
-}
-
-function startGame() {
-  var hero = new Hero(prompt('이름을 입력해주세요'), 100, 10);
-  logMessage(hero.name + '님이 모험을 시작합니다!');
-
-  while(!gameover) {
-    var monster = encounter();
-
-    logMessage(monster.name + '을(를) 마주쳤습니다. 전투를 시작합니다!', 'green');
-    battle = true;
-
-    while(battle) {
-      hero.attack(monster);
-      if(monster.hp > 0)
-        monster.attack(hero);
+        if(document.getElementById('menu_game').style.display === 'block') {
+          this.showBattleScreen();
+        } else {
+          this.showGameScreen();
+        }
+        return this;
+      }
+    };
+  };
+  return {
+    getinstance : function(name) {
+      if(!instance) {
+        instance = init(name);
+      }
+      return instance;
     }
+  };
+})();
+
+document.getElementById('screen_start').onsubmit = function(e) {
+  var name = document.getElementById('input_name').value;
+  e.preventDefault();
+
+  if(name && name.trim() && confirm(name + '으로 하시겠습니까?')) {
+    Game.getinstance(name).showHeroStat().toggleMenu();
+  } else {
+    alert('이름을 입력해주세요');
   }
+}
+
+document.getElementById('menu_game').onsubmit = function(e) {
+  var input = document.getElementById('input_menu');
+  var option = input.value;
+  e.preventDefault();
+  input.value = '';
+}
+
+document.getElementById('menu_battle').onsubmit = function(e) {
+  var input = document.getElementById('input_battle');
+  var option = input.value;
+  e.preventDefault();
+  input.value = '';
 }
